35291387c83db67a7b4856600901d01fc9eb3f43
Sufi <120452154+MarcalekM@users.noreply.github.com>
2024-02-09 11:04:58 +0100

    f8

--- Parkolohaz/Program.cs
+++ Parkolohaz/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Parkolohaz
+﻿using Parkolohaz;
+using System.Text;
+
+namespace Parkolohaz
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Szint> szintek = new();
+            using StreamReader sr = new(
+                path: @"..\..\..\src\parkolohaz.txt",
+                encoding: System.Text.Encoding.UTF8);
+            while(!sr.EndOfStream) szintek.Add(new(sr.ReadLine()));
+            foreach (var szint in szintek)
+            {
+                Console.WriteLine(szint.ToString());
+            }
+
+            int min = szintek[0]._szektorok.Values.Sum();
+            int index = 0;
+            for (int i = 1; i < szintek.Count; i++)
+            {
+                if (szintek[i]._szektorok.Values.Sum()  < min)
+                {
+                    min = szintek[i]._szektorok.Values.Sum();
+                    index = i;
+                }
+            }
+            Console.WriteLine($"\nA szektor, ahol a legkevesebb autó van a {szintek[index]._name}");
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Parkolohaz/Szint.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parkolohaz
+{
+    internal class Szint
+    {
+        public string _name;
+        public Dictionary<string, int> _szektorok = new Dictionary<string, int>();
+
+        public Szint(string sor)
+        {
+            var v = sor.Split(';').ToList();
+            _name = v[0];
+            int db = 0;
+            for (int i = 1; i < v.Count; i++)
+            {
+                 db = int.Parse(v[i]);
+                _szektorok.Add($"{i}. szektor", db);
+            }
+        }
+
+        public override string ToString()
+        {
+            string text = string.Empty;
+            if ( _name.Length < 7) text += _name + ",\t\t";
+            else text += _name + ",\t";
+            foreach (var szektor in _szektorok)
+            {
+                text += szektor.Value.ToString() + ",\t";
+            }
+            return text;
+        }
+    }
+}
