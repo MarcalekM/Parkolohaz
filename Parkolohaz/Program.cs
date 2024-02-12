827b9e06442e9a25591b623423330efe2e981eb4
Sufi <120452154+MarcalekM@users.noreply.github.com>
2024-02-12 11:11:29 +0100

    F10

--- Parkolohaz/Program.cs
+++ Parkolohaz/Program.cs
@@ -16,7 +16,7 @@ namespace Parkolohaz
             {
                 Console.WriteLine(szint.ToString());
             }
-
+            Console.WriteLine("\n8. felatat:");
             int min = szintek[0]._szektorok.Values.Sum();
             int index = 0;
             for (int i = 1; i < szintek.Count; i++)
@@ -27,9 +27,10 @@ namespace Parkolohaz
                     index = i;
                 }
             }
-            Console.WriteLine($"\nA szektor, ahol a legkevesebb autó van a {szintek[index]._name}");
+            Console.WriteLine($"\tA szektor, ahol a legkevesebb autó van a {szintek[index]._name}");
 
-            Console.WriteLine("\nSzintek és szektorok, ahol nincsenek autók:\n");
+            Console.WriteLine("\n9. feladat:");
+            Console.WriteLine("Szintek és szektorok, ahol nincsenek autók:");
             bool hiba = true;
             foreach (var szint in szintek)
             {
@@ -37,12 +38,34 @@ namespace Parkolohaz
                 {
                     if (szint._szektorok[$"{i+1}. szektor"] == 0)
                     {
-                        Console.WriteLine($"{szint._name}\t{i+1}. szektor");
+                        Console.WriteLine($"\t{szint._name}\t{i+1}. szektor");
                         hiba = false;
                     }
                 }
             }
             if ( hiba ) Console.WriteLine("Nincs olyan szektor, ahol nem parkol autó");
+
+            Console.WriteLine("\n10. feladat:");
+            double atlag = 0;
+            for (int i = 0; i < szintek.Count; i++)
+            {
+                atlag += szintek[i]._szektorok.Values.Sum();
+            }
+            atlag /= szintek.Count * 6;
+            atlag = Math.Round(atlag);
+            int atlagos = 0;
+            int atlagAlatt = 0;
+            int atlagFelett = 0;
+            foreach (var szint in szintek)
+            {
+                for (int i = 1; i < szint._szektorok.Count; i++)
+                {
+                    if (szint._szektorok[$"{i}. szektor"] == atlag)  atlagos++;
+                    else if(szint._szektorok[$"{i}. szektor"] < atlag) atlagAlatt++;
+                    else atlagFelett++;
+                }
+            }
+            Console.WriteLine($"Az átlagos autók száma:  {atlag}\n\t{atlagos} db szektorban van átlagos darabszámú autó\n\t{atlagAlatt} db szektorban van átlagosnál kevesebb darabszámú autó\n\t{atlagFelett}  db szektorban van átlagosnál nagyobb darabszámú autó");
             Console.ReadLine();
         }
     }
